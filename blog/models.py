92e448a61ad5f806f3b0d3e5dd11c4d7ccf10845
Janice Muia <janiceink001@gmail.com>
2020-05-26 00:38:44 +0300

    post model

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,4 @@ class Post (models.Model):
     slug = models.SlugField(max_length = 200, unique= True)
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name= 'blog_posts')
     updated_on = models.DateTimeField(auto_now = True)
-    c
+    content = models.TextField()
