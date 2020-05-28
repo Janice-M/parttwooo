b6ca0a15f93e1f43ba3bc03d35c5f25020fa9376
Janice Muia <janiceink001@gmail.com>
2020-05-28 22:48:46 +0300

    post detail

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ from .models import Post
 class PostList (generic.ListView):
     queryset = Post.object.filter(status=1).order_by('_created_on')
     template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
