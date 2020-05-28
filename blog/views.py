e44028eda64851def19f796588c240bc9743bfca
Janice Muia <janiceink001@gmail.com>
2020-05-28 22:47:06 +0300

    views

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
+
+class PostList (generic.ListView):
+    queryset = Post.object.filter(status=1).order_by('_created_on')
+    template_name = 'index.html'
